17c4ef7cb7543f24b475e07e0f9c1741fd9cd29f
miroslawdev <miras26@interia.pl>
2018-05-30 10:51:50 +0200

    pobieranie danych z tablicy

--- Program.cs
+++ Program.cs
@@ -9,7 +9,12 @@ namespace Aplikacja1
     class Program
     {
         static void Main(string[] args)
-        {
+        {            
+            // Uruchomic z konsoli z parametrem np:Mirek z lokalizacji -
+            //D:\CISCO\C#\projekty C#\KursCsharp\Aplikacja1\bin\Debug>Aplikacja1 Mirek
+            //uruchomienie w VS wywali wyjatek.
+            string name = args[0];
+            Console.WriteLine("Witaj, " + name);
         }
     }
 }
