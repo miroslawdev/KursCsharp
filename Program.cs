d804f971cfd959353c2b5e32e1d3c739a816b6cc
miroslawdev <miras26@interia.pl>
2018-05-31 15:13:45 +0200

    Color tekstu i petla for

--- Program.cs
+++ Program.cs
@@ -7,36 +7,40 @@ using System.Threading.Tasks;
 namespace Aplikacja1
 {
     class Program
-    {
+    {        
         static void Main(string[] args)
         {
-            Console.WriteLine("Wpisz swoje imię...");
-            string name = Console.ReadLine();
-            Console.WriteLine("Czesc, " + name);
-
-            Console.WriteLine("Wpisz swoj wiek...");
-            // int.Try Parse instrukcja zwraca typ bool i tu wyrzuca false gdy w "Wpisz swoj wiek..." zostaną użyte
-            //litery.
-            int wiek;
-            bool result = int.TryParse(Console.ReadLine(), out wiek);
-
-            if (wiek > 18)
-            {
-                Console.WriteLine("Super. Jak masz dowód to możesz kupić browce!");
-            }
-            else if (result == false)
-            {
-                Console.WriteLine("Uzywaj tylko cyfr!");
-            }
-            else
-            {
-                Console.WriteLine("Musisz jeszcze poczekac zanim kupisz alko!!!");
-
-            }
-           
-
-            Console.ReadLine();
             
+            //for (int i = 0; i < 10; i++) - taka petla wykona sie 10 razy
+            //for (;  ; ) - taka petla bedzie sie wykonywać w nieskończoność.
+            for (;  ; )
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Wpisz swoje imię...");
+                string name = Console.ReadLine();
+                Console.WriteLine("Czesc, " + name);
+
+                Console.WriteLine("Wpisz swoj wiek...");
+                // int.Try Parse instrukcja zwraca typ bool i tu wyrzuca false gdy w "Wpisz swoj wiek..." zostaną użyte
+                //litery.
+                int wiek;
+                bool result = int.TryParse(Console.ReadLine(), out wiek);
+
+                if (wiek > 18)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Super. Jak masz dowód to możesz kupić browce!");
+                }
+                else if (result == false)
+                {
+                    Console.WriteLine("Uzywaj tylko cyfr!");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Musisz jeszcze poczekac zanim kupisz alko!!!");
+                }
+            }            
         }
     }
 }
