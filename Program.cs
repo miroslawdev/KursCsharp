4ba343009763d34f327366599de19bfdb068a2cf
miroslawdev <miras26@interia.pl>
2018-05-31 15:44:01 +0200

    Refaktoryzacja

--- Program.cs
+++ Program.cs
@@ -10,41 +10,50 @@ namespace Aplikacja1
     {        
         static void Main(string[] args)
         {
-            
-            //for (int i = 0; i < 10; i++) - taka petla wykona sie 10 razy
-            //for (;  ; ) - taka petla bedzie sie wykonywać w nieskończoność.
+                      
             for (;  ; )
-            {                
-                Console.Write("Wpisz swoje imię: ");
-                //Console.Write - pozwala pisac w tej samej linii
-                string name = Console.ReadLine();
-                Console.WriteLine("Czesc, " + name);
+            {
+                Przywitanie();
+                Wiek();
+                Ustawienia();
 
-                Console.Write("Wpisz swoj wiek: ");
-                // int.Try Parse instrukcja zwraca typ bool i tu wyrzuca false gdy w "Wpisz swoj wiek..." zostaną użyte
-                //litery.
-                int wiek;
-                bool result = int.TryParse(Console.ReadLine(), out wiek);
+            }
+        }
 
-                if (wiek > 18)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Super. Jak masz dowód to możesz kupić browce!");
-                }
-                else if (result == false)
-                {
-                    Console.WriteLine("Uzywaj tylko cyfr!");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Musisz jeszcze poczekac zanim kupisz alko!!!");
-                }
-                Console.ReadKey();
-                Console.ResetColor();
-                Console.Clear();
+        private static void Ustawienia()
+        {
+            Console.ReadKey();
+            Console.ResetColor();
+            Console.Clear();
+        }
 
-            }            
+        private static void Wiek()
+        {
+            Console.Write("Wpisz swoj wiek: ");
+            int wiek;
+            bool result = int.TryParse(Console.ReadLine(), out wiek);
+
+            if (wiek > 18)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Super. Jak masz dowód to możesz kupić browce!");
+            }
+            else if (result == false)
+            {
+                Console.WriteLine("Uzywaj tylko cyfr!");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Musisz jeszcze poczekac zanim kupisz alko!!!");
+            }
+        }
+
+        private static void Przywitanie()
+        {
+            Console.Write("Wpisz swoje imię: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Czesc, " + name);
         }
     }
 }
